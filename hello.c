24c9843ab45249d0de54769b6659e3c8bad0085b
zwang86 <zwang86@huskers.unl.edu>
2019-01-17 08:10:56 -0600

    Initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Zeyu Wang
+ * Date: 2019/01/15
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Zeyu Wang\n");
+
+  return 0;
+}
